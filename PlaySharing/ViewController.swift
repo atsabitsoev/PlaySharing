f3f973d7daa615b2f2642112a528a064e4d97c0d
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-12-16 21:16:28 +0300

    Initial Commit

---
+++ PlaySharing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaySharing
+//
+//  Created by Ацамаз Бицоев on 16.12.2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
